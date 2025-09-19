9aa702a459b7bb3835dbe6f76e2fccd3ce8968d4
Ilya Shadryn <westdragon0@gmail.com>
2025-09-19 18:22:52 +0300

    sync change

--- postgresql/user-service/ddl/001_schema.sql
+++ postgresql/user-service/ddl/001_schema.sql
@@ -1,18 +1,18 @@
-\c service
+\c user_service
 
 CREATE SCHEMA data
     AUTHORIZATION user_service_user;
 
 CREATE TABLE data.users (
-    username varchar_ignorecase (50) NOT NULL PRIMARY KEY,
-    PASSWORD varchar_ignorecase (500) NOT NULL,
+    username varchar (50) NOT NULL PRIMARY KEY,
+    password varchar (500) NOT NULL,
     enabled boolean NOT NULL
 );
 
 CREATE TABLE data.authorities (
-    username varchar_ignorecase (50) NOT NULL,
-    authority varchar_ignorecase (50) NOT NULL,
-    CONSTRAINT fk_authorities_users FOREIGN KEY (username) REFERENCES users (username)
+    username varchar (50) NOT NULL,
+    authority varchar (50) NOT NULL,
+    CONSTRAINT fk_authorities_users FOREIGN KEY (username) REFERENCES data.users (username)
 );
 
 CREATE UNIQUE INDEX ix_auth_username ON data.authorities (username, authority);
